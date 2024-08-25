3fd6a965e2b51cc2fb224f724b72eee92b37e5de
arjit88 <arjitanand88@gmail.com>
2024-08-25 23:59:10 +0530

    Make edit option to edit your profile

--- chat/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ chat/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>We are on the profile page</div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt="avatar_icon"
+            />
+            upload profile image
+          </label>
+          <input type="text" placeholder="Your Name" required />
+          <textarea placeholder="Write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt="logo_icon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
