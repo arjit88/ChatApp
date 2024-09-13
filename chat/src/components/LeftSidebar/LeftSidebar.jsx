4b762d59f97dc221a9a32c4c2e68b5410da48b11
arjit88 <arjitanand88@gmail.com>
2024-09-14 03:20:30 +0530

    Deploy the project

--- chat/src/components/LeftSidebar/LeftSidebar.jsx
+++ chat/src/components/LeftSidebar/LeftSidebar.jsx
@@ -14,7 +14,7 @@ import {
   updateDoc,
   where,
 } from "firebase/firestore";
-import { db } from "../../config/firebase";
+import { db, logout } from "../../config/firebase";
 import { AppContext } from "../../context/AppContext";
 import { toast } from "react-toastify";
 
@@ -156,7 +156,7 @@ const LeftSidebar = () => {
             <div className="sub-menu">
               <p onClick={() => navigate("/profile")}>Edit Profile</p>
               <hr />
-              <p>Logout</p>
+              <p onClick={() => logout()}>Logout</p>
             </div>
           </div>
         </div>
